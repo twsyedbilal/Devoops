7da0cc5c762987739b13c5221f7a392ca24b560c
Bilal <twsyedbilal@gmail.com>
2021-11-01 11:27:19 +0530

    Intial code changes

---
+++ src/main/java/com/tw/devops/DevopsBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.tw.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsBuddyApplication.class, args);
+	}
+
+}
